75690ba34d224868aee764f298172b161fb56041
Ezra Chung <ezra.chung97@gmail.com>
2023-01-18 12:14:12 -0800

    Showing indicator button, but bottom overflowing still

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -5,57 +5,6 @@ import 'package:my_app/api_service.dart';
 import 'package:my_app/json.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-];
-
-final List<Widget> imageSliders = imgList
-    .map((item) => Container(
-          child: Container(
-            margin: EdgeInsets.all(5.0),
-            child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                child: Stack(
-                  children: <Widget>[
-                    Image.network(item, fit: BoxFit.cover, width: 1000.0),
-                    Positioned(
-                      bottom: 0.0,
-                      left: 0.0,
-                      right: 0.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(200, 0, 0, 0),
-                              Color.fromARGB(0, 0, 0, 0)
-                            ],
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                          ),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 20.0),
-                        child: Text(
-                          'No. ${imgList.indexOf(item)} image',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-          ),
-        ))
-    .toList();
-
 void main() {
   runApp(MyApp());
 }
@@ -125,7 +74,7 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CarouselWithIndicatorDemo(),
-            // BigCard(pair: pair),
+            BigCard(pair: pair),
             // Text(currFact.fact!.statement)
             SizedBox(height: 5),
             ElevatedButton(
@@ -155,33 +104,32 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-// class BigCard extends StatelessWidget {
-//   const BigCard({
-//     Key? key,
-//     required this.pair,
-//   }) : super(key: key);
+class BigCard extends StatelessWidget {
+  const BigCard({
+    Key? key,
+    required this.pair,
+  }) : super(key: key);
 
-//   final WordPair pair;
+  final WordPair pair;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     var theme = Theme.of(context);
-//     var style = theme.textTheme.displayMedium!.copyWith(
-//       color: theme.colorScheme.onPrimary,
-//     );
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
 
-//     return Card(
-//       color: theme.colorScheme.primary,
-//       child: Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Text(pair.asLowerCase, style: style),
-//       ),
-//     );
-//   }
-// }
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(pair.asLowerCase, style: style),
+      ),
+    );
+  }
+}
 
 // ################# Carousel ############################
-
 class CarouselWithIndicatorDemo extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -189,33 +137,53 @@ class CarouselWithIndicatorDemo extends StatefulWidget {
   }
 }
 
+
+// class CarouselDemo extends StatelessWidget {
+//   CarouselController buttonCarouselController = CarouselController();
 class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
   int _current = 0;
   final CarouselController _controller = CarouselController();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Carousel with indicator controller demo')),
-      body: Column(children: [
-        Expanded(
-          child: CarouselSlider(
-            items: imageSliders,
-            carouselController: _controller,
-            options: CarouselOptions(
+ @override
+  Widget build(BuildContext context) => Column(
+    children: <Widget>[
+      CarouselSlider(
+          items: [1,2,3,4,5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.amber
+                  ),
+                  child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                );
+              },
+            );
+          }).toList(),
+          options: CarouselOptions(
+              height: 400,
+              viewportFraction: 0.8,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
               autoPlay: true,
-                enlargeCenterPage: true,
-                aspectRatio: 2.0,
-                onPageChanged: (index, reason) {
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              enlargeFactor: 0.3,
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (index, reason) {
                   setState(() {
                     _current = index;
                   });
                 }),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: imgList.asMap().entries.map((entry) {
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [1,2,3,4,5].asMap().entries.map((entry) {
             return GestureDetector(
               onTap: () => _controller.animateToPage(entry.key),
               child: Container(
@@ -231,49 +199,12 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
               ),
             );
           }).toList(),
-        ),
-      ]),
-    );
-  }
+      ),
+      // ElevatedButton(
+      //   onPressed: () => buttonCarouselController.nextPage(
+      //       duration: Duration(milliseconds: 300), curve: Curves.linear),
+      //   child: Text('→'),
+      // )
+    ]
+  );
 }
-
-// class CarouselDemo extends StatelessWidget {
-//   CarouselController buttonCarouselController = CarouselController();
-
-//  @override
-//   Widget build(BuildContext context) => Column(
-//     children: <Widget>[
-//       CarouselSlider(
-//           items: [1,2,3,4,5].map((i) {
-//             return Builder(
-//               builder: (BuildContext context) {
-//                 return Container(
-//                   width: MediaQuery.of(context).size.width,
-//                   margin: EdgeInsets.symmetric(horizontal: 5.0),
-//                   decoration: BoxDecoration(
-//                     color: Colors.amber
-//                   ),
-//                   child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-//                 );
-//               },
-//             );
-//           }).toList(),
-//           options: CarouselOptions(
-//               height: 400,
-//               // aspectRatio: 16/9,
-//               viewportFraction: 0.8,
-//               initialPage: 0,
-//               enableInfiniteScroll: true,
-//               reverse: false,
-//               autoPlay: true,
-//               autoPlayInterval: Duration(seconds: 3),
-//               autoPlayAnimationDuration: Duration(milliseconds: 800),
-//               autoPlayCurve: Curves.fastOutSlowIn,
-//               enlargeCenterPage: true,
-//               enlargeFactor: 0.3,
-//               scrollDirection: Axis.horizontal,
-//           ),
-//       ),
-//     ]
-//   );
-// }

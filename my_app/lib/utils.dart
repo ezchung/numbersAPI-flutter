24ae87b2343f09199856f303ef378c155b203c2b
Spencer Taira <spencermtaira@gmail.com>
2023-01-19 15:05:43 -0800

    created util file to convert day of year to dates

---
+++ my_app/lib/utils.dart
@@ -0,0 +1,22 @@
+class Utils() {
+  dateFromDayOfYear(dayOfYear) {
+    day_of_year_to_month = {
+            1: 1,
+            32: 2,
+            61: 3,
+            92: 4,
+            122: 5,
+            153: 6,
+            183: 7,
+            214: 8,
+            245: 9,
+            275: 10,
+            306: 11,
+            336: 12,
+        }
+
+    if (dayOfYear == 0) {
+      num == 366;
+    }
+  }
+}
